dba01366d4fc2eae3f464cefc9ad496709e2be02
thelxz <thelxz@163.com>
2018-02-04 06:25:54 +0800

    add a 'always on top' check box for windows

--- src/regbits.cpp
+++ src/regbits.cpp
@@ -6,6 +6,11 @@
 #include <QPalette>
 #include <QLineEdit>
 #include "lineedits.h"
+#include <QCheckBox>
+
+#ifdef Q_OS_WIN
+#include <windows.h>
+#endif
 
 Regbits::Regbits(QWidget *parent)
     : QWidget(parent)
@@ -18,8 +23,12 @@ Regbits::Regbits(QWidget *parent)
     mainLayout->setSpacing(0);
     mainLayout->addWidget(intbits,0,0,1,4);
     mainLayout->addWidget(lineedits,0,5,2,3);
-    mainLayout->addWidget(bit_operate,1,0,1,4);
-
+    mainLayout->addWidget(bit_operate,1,0,1,1);
+#ifdef Q_OS_WIN
+    ckbox_always_on_top = new QCheckBox("OnTop");
+    mainLayout->addWidget(ckbox_always_on_top,1,1,1,1);
+    connect(this->ckbox_always_on_top,SIGNAL(stateChanged(int)),this,SLOT(set_alway_on_top(int)));
+#endif
     setLayout(mainLayout);
     connect(this->intbits,SIGNAL(value_changed(quint64)),this,SLOT(set_main_int(quint64)));
     connect(this->lineedits,SIGNAL(value_changed(quint64)),this,SLOT(set_main_int(quint64)));
@@ -27,6 +36,7 @@ Regbits::Regbits(QWidget *parent)
     this->set_main_int(0);
     //this->setFixedSize(this->width(),this->height());
     //this->setFixedSize(1200,150);
+
 }
 
 Regbits::~Regbits()
@@ -44,3 +54,14 @@ void Regbits::set_main_int(quint64 input_int)
     if(this->bit_operate->get_value()!=input_int)
         this->bit_operate->set_value(input_int);
 }
+
+#ifdef Q_OS_WIN
+void Regbits::set_alway_on_top(int cke_state){
+    if (cke_state==Qt::Unchecked){
+        SetWindowPos((HWND)winId(),HWND_NOTOPMOST,pos().x(),pos().y(),width(),height(),SWP_SHOWWINDOW);
+    }
+    else{
+        SetWindowPos((HWND)winId(),HWND_TOPMOST,pos().x(),pos().y(),width(),height(),SWP_SHOWWINDOW);
+    }
+}
+#endif
--- src/regbits.h
+++ src/regbits.h
@@ -6,6 +6,13 @@
 #include "lineedits.h"
 #include "bitoperate.h"
 
+/* Add always_on_top checkbox for windows, this is not nessary for Linux,
+ * most Window Manager can do this job well.
+*/
+#ifdef Q_OS_WIN32
+    class QCheckBox;
+#endif
+
 #define REG_BIT_NUM 64
 
 class Regbits : public QWidget
@@ -20,9 +27,17 @@ private:
     LineEdits * lineedits;
     BitOperate * bit_operate;
     quint64  main_int;
+
+#ifdef Q_OS_WIN32
+    QCheckBox * ckbox_always_on_top;
+public slots:
+    void set_alway_on_top(int);
+#endif
+
 signals:
 public slots:
     void set_main_int(quint64 input_int);
+
 };
 
 #endif // REGBITS_H
